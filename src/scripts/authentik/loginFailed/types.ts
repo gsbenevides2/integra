07c7a609bca0bbd2745ac9a66f838538bbf6b87c
Guilherme da Silva Benevides <git@gui.dev.br>
2026-06-28 15:44:01 -0300

    fix: change user_email and event_user_email types from email to string in loginFailedBody schema

--- src/scripts/authentik/loginFailed/types.ts
+++ src/scripts/authentik/loginFailed/types.ts
@@ -3,8 +3,8 @@ import z from "zod";
 export const loginFailedBody = z.object({
     body: z.string(),
     severity: z.string(),
-    user_email: z.email(),
+    user_email: z.string(),
     user_username: z.string(),
-    event_user_email: z.email(),
+    event_user_email: z.string(),
     event_user_username: z.string(),
 });
